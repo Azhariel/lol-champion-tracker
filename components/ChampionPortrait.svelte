7897edf97b68d9c5ed904ccd781f909819c0863c
Azhariel <Azhariel@users.noreply.github.com>
2023-02-15 01:15:07 -0300

    feat: gallery display for champions

--- components/ChampionPortrait.svelte
+++ components/ChampionPortrait.svelte
@@ -1,7 +1,54 @@
 <script lang="ts">
 	export let name: string
-	export let src: string
+	export let srcName: string
 </script>
 
-<p>{name}</p>
-<img {src} alt="Portrait of {name}" />
+<div>
+	<div class="card">
+		<div class="image-container">
+			<img
+				src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{srcName}_0.jpg"
+				alt={name}
+			/>
+		</div>
+	</div>
+	<div class="name">{name}</div>
+</div>
+
+<style>
+	.card {
+		width: 220px;
+		height: 400px;
+		box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+		border-radius: 10px;
+		overflow: hidden;
+		transition: box-shadow 0.3s;
+	}
+
+	.card:hover {
+		box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+	}
+
+	.image-container {
+		height: 100%;
+		position: relative;
+		overflow: hidden;
+	}
+
+	img {
+		height: 100%;
+		width: 100%;
+		object-fit: fill;
+		transition: transform 0.3s;
+	}
+
+	.card:hover img {
+		transform: scale(1.1);
+	}
+
+	.name {
+		padding: 0.5rem;
+		font-size: 1.2rem;
+		text-align: center;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import ChampionPortrait from '../../components/ChampionPortrait.svelte'
+	import champions from '../../data/champions.json'
+</script>
+
+<svelte:head>
+	<title>LoL Champion Challenge</title>
+	<meta name="description" content="A test app for the LoL Champion Challenge" />
+</svelte:head>
+<h1>LoL Champion Challenge</h1>
+
+<div class="gallery">
+	{#each Object.keys(champions) as champion}
+		<ChampionPortrait name={champions[champion].name} srcName={champion} />
+	{/each}
+</div>
+
+<style>
+	.gallery {
+		display: flex;
+		flex-wrap: wrap;
+		gap: 1rem;
+	}
+	h1 {
+		text-align: center;
+	}
+</style>
