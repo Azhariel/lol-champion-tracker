89ed566a8290d48ed33ea957ca99d115ea1c95b5
Azhariel <Azhariel@users.noreply.github.com>
2023-02-16 01:50:58 -0300

    ✨ check champions and save to local storage

--- components/ChampionPortrait.svelte
+++ components/ChampionPortrait.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
 	export let name: string
 	export let srcName: string
+	const handleClick = (event: { target: any }) => {
+		if (localStorage.getItem(event.target.id) === 'true') {
+			localStorage.setItem(event.target.id, 'false')
+			event.target.style.filter = 'brightness(0.2)'
+			return
+		}
+		localStorage.setItem(event.target.id, 'true')
+		event.target.style.filter = 'brightness(1)'
+	}
+	let brightness = 0.2
+	$: {
+		if (typeof window !== 'undefined')
+			localStorage.getItem(srcName) === 'true' ? (brightness = 1) : (brightness = 0.2)
+	}
 </script>
 
 <div>
@@ -9,6 +23,10 @@
 			<img
 				src="http://ddragon.leagueoflegends.com/cdn/img/champion/loading/{srcName}_0.jpg"
 				alt={name}
+				id={srcName}
+				on:click={handleClick}
+				on:keydown={handleClick}
+				style="filter: brightness({brightness})"
 			/>
 		</div>
 	</div>
